ca70b16b3abd60fe7e193f1b06a006961d17aa2d
bhavesh625 <165816523+bhavesh625@users.noreply.github.com>
2024-04-10 16:58:22 +0000

    done

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -11,22 +11,33 @@ export const Team = (props) => {
             dapibus leonec.
           </p>
         </div>
-        <div id="row">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
-                  <div className="thumbnail">
-                    {" "}
-                    <img src={d.img} alt="..." className="team-img" />
-                    <div className="caption">
-                      <h4>{d.name}</h4>
-                      <p>{d.job}</p>
-                    </div>
-                  </div>
-                </div>
-              ))
-            : "loading"}
+        <div id="row" className="row">
+  {/* Rendering the first three blocks */}
+  {props.data && props.data.length >= 3 && props.data.slice(0, 3).map((d, i) => (
+    <div key={`${d.name}-${i}`} className="col-lg-4 col-md-4 col-sm-6 team">
+      <div className="thumbnail">
+        <img src={d.img} alt="..." className="team-img" />
+        <div className="caption">
+          <h4>{d.name}</h4>
+          <p>{d.job}</p>
         </div>
+      </div>
+    </div>
+  ))}
+  {/* Rendering the additional blocks or placeholders */}
+  {Array.from({ length: 2 }).map((_, i) => (
+    <div key={`placeholder-${i}`} className="col-lg-6 col-md-6 col-sm-6 team">
+      <div className="thumbnail">
+        <img src="placeholder_image_url" alt="..." className="team-img" />
+        <div className="caption">
+          <h4>Placeholder Name</h4>
+          <p>Placeholder Job</p>
+        </div>
+      </div>
+    </div>
+  ))}
+</div>
+
       </div>
     </div>
   );
